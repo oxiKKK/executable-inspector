e026180310d67bb85b222603acbdf8fc0c5beec5
oxiKKK <danekgavlas1@email.cz>
2023-11-26 11:12:44 +0100

    add file headers

--- src/app/precompiled.cc
+++ src/app/precompiled.cc
@@ -1,1 +1,10 @@
+/***
+*	executable-inspector developed by oxiKKK
+*
+*	Copyright (c) 2022-present
+*
+*	For licensing information, see the "LICENSE" file
+*	included in project root directory.
+****/
+
 #include "precompiled.h"
---
+++ src/app/precompiled.h
@@ -0,0 +1,8 @@
+/***
+*	executable-inspector developed by oxiKKK
+*
+*	Copyright (c) 2022-present
+*
+*	For licensing information, see the "LICENSE" file
+*	included in project root directory.
+****/
